d58b6245c24aba4a0ce526e0268e528ec86a57f0
Jaime Enrique Trujillo MOntoya <jetrujillom@gmail.com>
2020-09-26 18:03:07 -0500

    Area_Circulo.js

--- Ejercicio3.js
+++
@@ -1,8 +0,0 @@
-console.log("Números Primos");
-console.log(" ");
-
-var num = 9;
-var X =0;
-var OK = false;
-
-OK = num.mod X;
--- repository/Ejercicio3.js
+++ repository/Ejercicio3.js
@@ -1,3 +1,12 @@
-console.log("Números Primos");
-console.log(" ");
+    console.log("Area Circulo");
+    console.log(" ");
 
+    function area(radio)
+    {
+      let conpi = 3.14159265358979;
+      let area1=conpi*radio^2;
+      return area1;
+    }
+
+    
+    console.log(`El área del circulo es ${area(2)}`);
