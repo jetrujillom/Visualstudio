ab963a8dcb74f0678ee94e779292d383acc096c2
Jaime Enrique Trujillo MOntoya <jetrujillom@gmail.com>
2020-09-29 12:53:34 -0500

    ahorro

--- repository/Ejercicio2.js
+++ repository/Ejercicio2.js
@@ -1,12 +1,29 @@
     console.log("Ahorro mensual");
     console.log(" ");
-    var ahorro =0; 
+    
+    function ahorro()
+    {
+        let count =0;
+        return {
+            increment: function(){
+                count++;
+                ahorro1 = ahorro1 + ahmes[i];
+                return ahorro;
+            },
+            show: function(){
+                return count;
+            } 
+        }
+    }
+    
+    var count = ahorro(); 
+    var ahorro1=0;
     var mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
     var ahmes = [100000, 150000, 80000, 120000, 200000, 170000, 100000, 90000, 95000, 140000, 250000, 220000];
     
     for (var i=0; i<12; i++) {
-        ahorro = ahorro + ahmes[i];
-        console.log("en ", mes[i], "ahorró ", ahmes[i]," lleva ahorrado ", ahorro);
+        console.log(`en ${mes[count.show()]} tenía ${ahorro1} pesos, y ahorró ${ahmes[count.show()]}`);
+        count.increment();
     }
 
-    console.log("El total ahorrado en el año es = ", ahorro);
+    console.log("El total ahorrado en el año es = ", ahorro1);
