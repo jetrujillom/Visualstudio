7a3e4318f7e1748f67186c3ee529f6a284c47c7c
Jaime Enrique Trujillo MOntoya <jetrujillom@gmail.com>
2020-10-07 14:49:07 -0500

    corrección ejercicio

--- repository/3-12.js
+++ repository/3-12.js
@@ -1,39 +1,24 @@
-
-function Contador(){
-    let count = contador;
-        return {
-            Primos: function(){
-                count++;
-                if (count > 0 && count <4 || count === 5){
-                    NumerosPrimos.push(count);
-                    p++;
-                } else if ((count % 2 === 0) || (count % 3 === 0) || (count % 5 === 0) ){
-                    
-                 } else {   
-                    NumerosPrimos.push(count);
-                    p++;
-                 }
-                return Contador;
-            },
-            show: function(){
-                return count;
-            } 
-        }
-}
-
- let numero =7; 
- let contador =0;
- let NumerosPrimos = [];
- var count = Contador();   
- let p=1;
-
- while(p<=numero){
-    count.Primos();
-
-}
-
-console.log(`los primeros ${numero} numeros primos son ${NumerosPrimos}`);
-
-
-
-    
+function esPrimo(numero) {
+    if (numero > 0 && numero < 4 || numero === 5) {
+      return true;
+    }
+    if ( (numero % 2 === 0) || (numero % 3 === 0) || (numero % 5 === 0) ) {
+      return false;
+    }
+    return true;
+  }
+  
+  function cantidadPrimos(cantidad) {
+      const primos = [];
+      let count = 1;
+      while ( primos.length < cantidad ) {
+        if ( esPrimo(count))
+          primos.push(count);
+          count++;
+      }
+      return primos;
+  }
+  const numero = 7;
+  const numerosPrimos = cantidadPrimos(numero);
+  console.log(`los primeros ${numero} numeros primos son ${numerosPrimos}`);
+  
