eb2decef731982458636c8ec254c6c3c9adc5b3e
Jaime Enrique Trujillo MOntoya <jetrujillom@gmail.com>
2020-10-07 15:37:35 -0500

    cCalificacion promedio

---
+++ repository/1-5.js
@@ -0,0 +1,7 @@
+function CalificacionPromedio(cal1,cal2,cal3)
+{
+   let promedio= (cal1+cal2+cal3)/3;
+   return promedio;
+}
+
+console.log(`La calificación promedio es: ${CalificacionPromedio(5.0,2.9,4.2)}`);
